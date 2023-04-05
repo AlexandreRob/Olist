869fa9126aca994714ce3a27c8885f65643b22cd
Saia Ralandison <ralandison.saia@gmail.com>
2023-04-05 11:10:04 +0400

    Inst Django-restframework

--- src/src/settings.py
+++ src/src/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'frontEnd',
+    'user',
+    'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
