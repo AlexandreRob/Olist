3b1e5ce95fbb5a97a3d6dacea80ca849442fa290
Saia Ralandison <ralandison.saia@gmail.com>
2023-04-06 08:24:34 +0400

    ajout accueil html

--- src/frontEnd/views.py
+++ src/frontEnd/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+def connexion(request):
+  template = loader.get_template('accueil.html')
+  return HttpResponse(template.render())
