b9ca750e1f3e5bd91148532d1b4fed3944a591d0
AlexandreRob <alexandre.robert974@hotmail.fr>
2023-05-01 20:15:29 +0400

    Create script.js

---
+++ src/frontend/static/js/script.js
@@ -0,0 +1,37 @@
+// const API_URL = "http://127.0.0.1:8000/api/";
+
+// fetch(API_URL)
+//   .then(response => response.json())
+//   .then(data => console.log(data))
+//   .catch(error => console.error(error));
+
+// $.ajax({
+//   url: API_URL + 'stateViewset/',
+//   method: 'GET',
+//   dataType: 'json'
+// })
+// .then(function(data) {
+//   console.log(data);
+//   data.forEach(element => {
+//     console.log(
+//       element.state_id,
+//       element.location,
+//       element.state_name,
+//     )
+    
+//   });
+
+// })
+// .fail(function(jqXHR, textStatus, errorThrown) {
+//   console.error(errorThrown);
+// });
+
+// const { createApp } = Vue
+
+// createApp({
+//   data() {
+//     return {
+//       message: 'Hello Vue!'
+//     }
+//   }
+// }).mount('#app')
